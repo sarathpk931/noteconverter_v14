2036bd1f37381e9d23a8dda55d073729feb9bfa2
somimphasis <somi.ajesh@mphasis.com>
2023-05-11 20:57:51 +0530

    generating job id

--- src/app/services/job.service.ts
+++ src/app/services/job.service.ts
@@ -63,10 +63,11 @@ export class JobService {
         });
     }
 
-    generateNewJobID() {
+    generateNewJobID():string {
       const guid = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
       const r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
       return v.toString(16);
       });
+      return guid;
     }
 }
