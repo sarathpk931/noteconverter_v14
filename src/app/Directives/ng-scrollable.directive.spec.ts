5278ea162780bb572e1ce1ff7bed66e6981db629
Saransh Dwivedi <saransh.dwivedi@we3.tech>
2023-05-30 17:27:31 +0530

    ng scroll directive

---
+++ src/app/Directives/ng-scrollable.directive.spec.ts
@@ -0,0 +1,8 @@
+import { NgScrollableDirective } from './ng-scrollable.directive';
+
+describe('NgScrollableDirective', () => {
+  it('should create an instance', () => {
+    const directive = new NgScrollableDirective();
+    expect(directive).toBeTruthy();
+  });
+});
