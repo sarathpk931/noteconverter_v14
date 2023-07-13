71ebfda485e21aeae32ef27ba4f9c75c2904c52f
Aakash Jain Mphasis <akashjain.jain6@gmail.com>
2023-07-13 17:06:56 +0530

    css fix

--- src/app/services/modal.service.ts
+++ src/app/services/modal.service.ts
@@ -96,58 +96,65 @@ export class ModalService {
     this.fromData.next(data);
   }
 
+  setPositionOfDialog() {
+
+  }
+  
   public openModal(component : any,dialog_postion:any,  clickPosition:any){
     document.querySelectorAll("#modal_arrow").forEach(e => e.parentNode.removeChild(e));
     this.dialog.closeAll();
     this.dialog.openDialogs.pop();
 
     let dialogRef = this.dialog.open(component,{
-      position: dialog_postion,
+      position: { ...dialog_postion, top: '100%' },
       panelClass: `custom-dialog-position`,
       data: { clickPosition, additionalInfo: `calc(${clickPosition.y}px - ${dialog_postion.top})`}
     });
-    
 
     dialogRef.afterOpened().subscribe(result => {
       const customDialogPosition : HTMLElement = document.querySelector(".custom-dialog-position");
-      customDialogPosition.style.top = dialog_postion.top;
+      let horizontalPosition = '';
       if(dialog_postion.left) {
-        customDialogPosition.style.left = dialog_postion.left;
+        horizontalPosition = `left: ${dialog_postion.left}`;
       } else if(dialog_postion.right) {
-        customDialogPosition.style.right = dialog_postion.right;
+        horizontalPosition = `right: ${dialog_postion.right}`;
       }
-    });
+
+      customDialogPosition.style.cssText = `margin: 0!important; top: ${dialog_postion.top};${horizontalPosition};`
     
-    const arrowsSize = 20;
-    const common_arrow_style = `
-      position: absolute; 
-      width: 0; 
-      height: 0; 
-      border-top: ${arrowsSize}px solid transparent; 
-      border-bottom: ${arrowsSize}px solid transparent; 
-      z-index: 1000; 
-      top: ${clickPosition.y  - arrowsSize}px;
-    `
-    const modalArrow = document.createElement("div");
-    const modalBoxShadow = 2;
-    modalArrow.id = 'modal_arrow';
-
-    if (clickPosition.showLeftArrow) {
-      modalArrow.style.cssText += `
-      ${common_arrow_style}
-      border-right: ${arrowsSize}px solid #ddd;
-      left: calc(${dialog_postion.left} - ${arrowsSize}px + ${modalBoxShadow}px);
-    `;
-    } else {
-      modalArrow.style.cssText += `
-      ${common_arrow_style}
-      border-left: ${arrowsSize}px solid #ddd;
-      right: calc(${clickPosition.xForRightArrow}px - ${arrowsSize}px + ${modalBoxShadow}px);
-    `;
-    }
-
-    const popupContainer = document.querySelector('.cdk-overlay-container');
-    popupContainer.appendChild(modalArrow);
+    
+      const arrowsSize = 20;
+      const common_arrow_style = `
+        position: absolute; 
+        width: 0; 
+        height: 0; 
+        border-top: ${arrowsSize}px solid transparent; 
+        border-bottom: ${arrowsSize}px solid transparent; 
+        z-index: 1000; 
+        top: ${clickPosition.y  - arrowsSize}px;
+      `
+      const modalArrow = document.createElement("div");
+      const modalBoxShadow = 2;
+      modalArrow.id = 'modal_arrow';
+  
+      if (clickPosition.showLeftArrow) {
+        modalArrow.style.cssText += `
+        ${common_arrow_style}
+        border-right: ${arrowsSize}px solid #ddd;
+        left: calc(${dialog_postion.left} - ${arrowsSize}px + ${modalBoxShadow}px);
+      `;
+      } else {
+        modalArrow.style.cssText += `
+        ${common_arrow_style}
+        border-left: ${arrowsSize}px solid #ddd;
+        right: calc(${clickPosition.xForRightArrow}px - ${arrowsSize}px + ${modalBoxShadow}px);
+      `;
+      }
+  
+      const popupContainer = document.querySelector('.cdk-overlay-container');
+      popupContainer.appendChild(modalArrow);
+    
+    });
    
     dialogRef.afterClosed()
     .pipe(finalize(() => {
