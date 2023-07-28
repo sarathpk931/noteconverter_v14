08ab396789a8b191a3cb7d9d21962efc6af17702
Saransh Dwivedi <dwivedisaransh1992@gmail.com>
2023-07-28 16:46:08 +0530

    text field directive onblur

--- src/app/Directives/text-field.directive.ts
+++ src/app/Directives/text-field.directive.ts
@@ -10,14 +10,23 @@ export class TextFieldDirective {
   @Input() initialValue: string; 
   @Input() readOnlyClass: string;
   @Input() editableClass : string;
+  @Input() locked: boolean = false;
+  
 
   private editMode = false; 
   private editableText: string;
+  editing: boolean = false;
+  currentTextField: ElementRef | null = null;
   
   @HostBinding ('class') hostClasses = '';
   //@HostBinding (editableClass) editmode = true;
 
   constructor(private elementRef: ElementRef<HTMLInputElement>) { }
+
+  ngOnInit(){
+    this.elementRef.nativeElement.querySelector('input').setAttribute('readonly', 'true');
+    
+  }
 
   @HostListener('click') onClick() { 
     this.editMode = true; 
@@ -29,6 +38,48 @@ export class TextFieldDirective {
     childNode.select(); 
   } 
 
+  @HostListener('tap', ['$event'])
+  @HostListener('click', ['$event'])
+  onTextFieldClick(event: Event) {
+    this.currentTextField = this.elementRef;
+
+    if (
+      event.target instanceof Element &&
+      event.target.closest('.wrapper') !== null &&
+      event.type === 'click'
+    ) {
+      event.stopPropagation();
+      event.preventDefault();
+      return;
+    }
+
+    this.elementRef.nativeElement.querySelector('input').removeAttribute('readonly');
+
+    if (!event.defaultPrevented && !this.locked) {
+      const alreadyEditing = this.editing;
+      this.scrollTextFieldIntoView();
+
+      setTimeout(() => {
+        this.editing = true;
+      }, 50);
+
+      setTimeout(() => {
+        this.elementRef.nativeElement.querySelector('input').focus();
+        if (!alreadyEditing) {
+          this.elementRef.nativeElement.querySelector('input').select();
+        }
+      }, 200);
+
+      document.removeEventListener('tap', this.outsideClick);
+      document.removeEventListener('click', this.outsideClick);
+      document.addEventListener('tap', this.outsideClick);
+      document.addEventListener('click', this.outsideClick);
+
+      event.stopPropagation();
+      event.preventDefault();
+    }
+  }
+
   @HostListener('blur') onBlur() { 
     this.hostClasses =`${this.readOnlyClass}`;
     this.editMode = false; 
@@ -38,5 +89,27 @@ export class TextFieldDirective {
     this.elementRef.nativeElement.blur(); 
   } 
 
+  /* handleKeyEnter(key: KeyboardEvent) {
+    if (key.key === 'Enter') {
+      key.stopPropagation();
+      key.preventDefault();
+      this.outsideClick();
+      return false;
+    }
+  } */
+
+  outsideClick(event: MouseEvent) {
+    const targetNode = event.target as Node;
+
+    if (this.editMode && !this.elementRef.nativeElement.contains(targetNode)) {
+      this.editMode = false;
+      this.hostClasses = `${this.readOnlyClass}`;
+    }
+
+  }
+
+  scrollTextFieldIntoView() {
+    // Implement the logic for scrolling the element into view (same as in the AngularJS code)
+  }
 }
 
